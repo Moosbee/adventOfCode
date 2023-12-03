c7a76e1db4330193d2ccddbd8e708b2609abce38
Moosbee <bernhardr91104@gmail.com>
2023-12-03 20:16:28 +0100

    day 3 part 2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,27 +1,30 @@
-use std::fs;
 use regex::Regex;
+use std::fs;
 
 fn main() {
-    let input =
-        fs::read_to_string("./input.txt").expect("Should have been able to read the file");
+    let input = fs::read_to_string("./input.txt").expect("Should have been able to read the file");
 
+    part1(&input);
+
+    part2(&input);
+}
+
+fn part1(input: &String) {
     let input_lines: Vec<&str> = input.lines().collect();
 
     println!("Files Lines {}", input_lines.clone().len());
 
     let regex = Regex::new(r"[^.\d]").unwrap();
 
-    println!("regex:{:?}",regex);
+    println!("regex:{:?}", regex);
 
-    // let line_chars=input_lines.map(|x| x.chars());
-
-    let mut total=0;
+    let mut total = 0;
 
     for (index_line, chars) in input_lines.iter().enumerate() {
         let mut had_number = false;
         let mut start_index = 0;
         for (index_char, cha) in chars.chars().enumerate() {
-            // println!("Char:{} {} {}", cha, index_char, index_line);
+            println!("Char:{} {} {}", cha, index_char, index_line);
 
             let is_num = cha.is_ascii_digit();
 
@@ -61,11 +64,14 @@ fn main() {
                     str_down = "";
                 }
 
-                let str_all= str_up.to_owned()+str_middle+str_down;
+                let str_all = str_up.to_owned() + str_middle + str_down;
 
                 println!(
                     "Area around it:\n{}\n{}\n{}\n{}",
-                    &str_up, &str_middle, &str_down,regex.is_match(&str_all)
+                    &str_up,
+                    &str_middle,
+                    &str_down,
+                    regex.is_match(&str_all)
                 );
 
                 if regex.is_match(&str_all) {
@@ -73,12 +79,121 @@ fn main() {
                 }
 
                 println!("-------------------------------")
-
             }
 
             had_number = is_num;
         }
+      }
+      println!("Total: {}", total);
+}
 
-        println!("Total: {}",total);
+#[derive(Debug)]
+pub struct Star {
+    index_line: usize,
+    index_char: usize,
+    adjacent_numbers: Vec<i32>,
+}
+
+fn part2(input: &String) {
+    let input_lines: Vec<&str> = input.lines().collect();
+
+    println!("Files Lines {}", input_lines.clone().len());
+
+    let regex = Regex::new(r"[\*]").unwrap();
+
+    println!("regex:{:?}", regex);
+
+    let mut stars: Vec<Star> = vec![];
+
+    for (index_line, chars) in input_lines.iter().enumerate() {
+        for (index_char, cha) in chars.chars().enumerate() {
+            if cha == '*' {
+                let new_star = Star {
+                    index_line,
+                    index_char,
+                    adjacent_numbers: vec![],
+                };
+                stars.push(new_star);
+            }
+        }
     }
+
+    println!("Stars:{:?}", &stars.len());
+
+    for (index_line, chars) in input_lines.iter().enumerate() {
+        let mut had_number = false;
+        let mut start_index = 0;
+        for (index_char, cha) in chars.chars().enumerate() {
+            println!("Char:{} {} {}", cha, index_char, index_line);
+
+            let is_num = cha.is_ascii_digit();
+
+            if is_num && !had_number {
+                start_index = index_char;
+            }
+
+            if had_number && !is_num {
+                let number: i32 = chars[start_index..index_char]
+                    .parse()
+                    .ok()
+                    .unwrap_or_default();
+
+                let area_start = if start_index == 0 {
+                    start_index
+                } else {
+                    start_index - 1
+                };
+
+                let area_end = chars.len().min(index_char + 1);
+
+                let line_start = if index_line == 0 {
+                    index_line
+                } else {
+                    index_line - 1
+                };
+
+                let line_end = chars.len().min(index_line + 1);
+
+                for star in &mut stars {
+                    println!(
+                        "Num:{} near Star {} is {} than {} and {} than {}, {} is {} than {} and {} than {}",
+                        number,
+                        star.index_line,
+                        star.index_line >= line_start,
+                        line_start,
+                        star.index_line <= line_end,
+                        line_end,
+                        star.index_char,
+                        star.index_char >= area_start,
+                        area_start,
+                        star.index_char < area_end,
+                        area_end
+                    );
+
+                    if star.index_line >= line_start
+                        && star.index_line <= line_end
+                        && star.index_char >= area_start
+                        && star.index_char < area_end
+                    {
+                        star.adjacent_numbers.push(number);
+                    }
+                }
+            }
+
+            had_number = is_num;
+        }
+    }
+
+    println!("All Stars:{}", &stars.len());
+
+    let mut sum = 0;
+
+    for str in stars {
+      println!("Star {} {} has {} adjacent numbers",str.index_line,str.index_char,str.adjacent_numbers.len());
+        if str.adjacent_numbers.len() == 2 {
+            sum = sum + (str.adjacent_numbers[0] * str.adjacent_numbers[1]);
+        }
+    }
+
+    println!("All gear ratios: {}", sum);
 }
