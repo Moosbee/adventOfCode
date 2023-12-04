f235c005a27c065c75a39eb3cb9cca82c6740291
Moosbee <bernhardr91104@gmail.com>
2023-12-04 15:15:42 +0100

    day 4 init

---
+++ day4/src/main.rs
@@ -0,0 +1,12 @@
+use std::fs;
+
+fn main() {
+  let input =
+  fs::read_to_string("./test_input.txt").expect("Should have been able to read the file");
+
+let input_lines = input.lines();
+
+println!("Files Lines {}", input_lines.clone().count());
+
+
+}
