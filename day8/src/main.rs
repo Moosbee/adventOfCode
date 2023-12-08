2c3514c9a55a8bf2585c4f1d5016f8d8a01267b7
Moosbee <bernhardr91104@gmail.com>
2023-12-08 15:26:24 +0100

    day 8 part 2 the brute force way

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{fs, time::Instant};
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Node {
@@ -33,34 +33,91 @@ fn main() {
         })
         .collect();
 
-    nodes.sort();
+    nodes.sort_by(|f, g| f.id.cmp(&g.id));
 
     println!("Nodes:{:?}", nodes);
 
-    let mut new_node_id: &String = &"AAA".to_string();
+    let mut current_nodes: Vec<&Node> = nodes
+        .iter()
+        .filter_map(|node| {
+            if node.id.ends_with('A') {
+                Some(node)
+            } else {
+                None
+            }
+        })
+        .collect();
+
+    println!("Current Nodes:{:?}", current_nodes);
+
     let mut turn_index: usize = 0;
-    let mut times = 0;
+    let mut times: u128 = 0;
+
+    let start = Instant::now();
 
     loop {
-        let new_node = nodes.iter().find(|f| f.id.eq(new_node_id)).unwrap();
-        let turn = turn_signals[turn_index];
+        current_nodes = current_nodes
+            .iter()
+            .map(|node| get_next_node(node, turn_index, &nodes, &turn_signals))
+            .collect();
+
         turn_index = (turn_index + 1) % turn_signals.len();
 
-        if turn == 'R' {
-            new_node_id = &new_node.right;
-        } else {
-            new_node_id = &new_node.left;
+        times = times + 1;
+
+        if times % 10000000 == 0 {
+            let count = current_nodes
+                .iter()
+                .filter(|node| node.id.ends_with('Z'))
+                .count();
+
+            println!(
+                "Node count: {} r nodes: {} Current Nodes:{:?} iteration:{:?}",
+                current_nodes.len(),
+                count,
+                current_nodes,
+                times
+            );
         }
 
-        println!(
-            "Node_traverse: {:?} Turn: {} i: {} new node id: {} times: {}",
-            new_node, turn, turn_index, new_node_id, times
-        );
-        if new_node.id == "ZZZ" {
+        // if count == current_nodes.len() {
+        //     break;
+        // }
+
+        if current_nodes.iter().all(|node| node.id.ends_with('Z')) {
             break;
         }
-        times = times + 1;
     }
 
-    println!("Times: {}", times);
+    println!(
+        "Times: {} End: {:?} took: {:?}",
+        times,
+        current_nodes,
+        start.elapsed()
+    );
+}
+
+fn get_next_node<'a>(
+    old_node: &'a Node,
+    turn_index: usize,
+    nodes: &'a Vec<Node>,
+    turn_signals: &'a Vec<char>,
+) -> &'a Node {
+    let turn = turn_signals[turn_index];
+
+    let new_node_id: &String;
+
+    if turn == 'R' {
+        new_node_id = &old_node.right;
+    } else {
+        new_node_id = &old_node.left;
+    }
+    let new_node = nodes.binary_search_by(|f| f.id.cmp(new_node_id)).unwrap();
+
+    // println!(
+    //     "Node traverse from: {:?} to {:?} Turn: {} i: {} new node id: {} times: {}",
+    //     old_node, new_node, turn, turn_index, new_node_id, times
+    // );
+
+    &nodes[new_node]
 }
