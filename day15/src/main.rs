97959d793e517532999c7e1319599a8ccbdb9aa1
Moosbee <bernhardr91104@gmail.com>
2023-12-15 11:38:38 +0100

    day 15 part 1

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -9,5 +9,25 @@ fn main() {
 
     println!("Files Lines {}", input_parts.clone().count());
 
+    let mut sum: i32 = 0;
+
+    println!("Hash of 'HASH' is {}", gen_hash("HASH"));
+
+    for part in input_parts {
+        let hash: i32 = gen_hash(part).into();
+        println!("Hash of '{}' is {}", part, hash);
+        sum = sum + hash;
+    }
+
     println!("Erg Part 1: {} Took {:?}", 0, start.elapsed())
+}
+
+
+fn gen_hash(text: &str) -> i32 {
+  let mut sum = 0;
+  for chr in text.chars() {
+      let asci_code: i32 = chr as i32;
+      sum = ((sum + asci_code) * 17) % 256;
+  }
+  sum
 }
