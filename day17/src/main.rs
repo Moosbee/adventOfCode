d28360378bab25cb93a9efb15debedb5c59252b0
Moosbee <bernhardr91104@gmail.com>
2023-12-18 12:32:57 +0100

    day 18 init

---
+++ day17/src/main.rs
@@ -0,0 +1,19 @@
+use std::fs;
+
+use std::time::Instant;
+
+fn main() {
+    let start = Instant::now();
+    let input = fs::read_to_string("./input.txt").expect("Should have been able to read the file");
+
+    let input_lines = input.lines();
+
+    println!("Files Lines {}", input_lines.clone().count());
+
+    let heat: Vec<Vec<char>> = input_lines
+        .map(|line| line.chars().filter_map(|f| Some(f)).collect())
+        .collect();
+
+    println!("heat {:?}", heat);
+    println!("Took {:?}", start.elapsed())
+}
