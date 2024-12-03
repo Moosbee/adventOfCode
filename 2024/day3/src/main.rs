48ecde2ad51d88e70b7ca7b524ba2a73b9318e88
Moosbee <moosbeee@gmail.com>
2024-12-03 08:47:25 +0100

    day 2 part 2

--- 2024/day3/src/main.rs
+++ 2024/day3/src/main.rs
@@ -1,16 +1,31 @@
-use std::fs;
+use std::{fs, time::Instant};
 
 use regex::Regex;
 
 fn main() {
     let input =
-        fs::read_to_string("./test_input.txt").expect("Should have been able to read the file");
+        fs::read_to_string("./test_input_2.txt").expect("Should have been able to read the file");
     let input = fs::read_to_string("./input.txt").expect("Should have been able to read the file");
 
     let input_lines = input.split('\n');
 
     println!("Files Lines {}", input_lines.clone().count());
 
+    let start = Instant::now();
+
+    let part1_sum = part1(input.clone());
+
+    let part2_sum = part2(input.clone());
+
+    println!(
+        "Part 1: {} Part 2 {} Took {:?}",
+        part1_sum,
+        part2_sum,
+        start.elapsed()
+    );
+}
+
+fn part1(input: String) -> i32 {
     let mul_regex = Regex::new(r"mul\([0-9]{1,3},[0-9]{1,3}\)").unwrap();
 
     let instructions = mul_regex
@@ -33,5 +48,39 @@ fn main() {
         }
     }
 
-    println!("Sum: {}", sum);
+    sum
+}
+
+fn part2(input: String) -> i32 {
+    let mul_do_regex = Regex::new(r"mul\([0-9]{1,3},[0-9]{1,3}\)|do\(\)|don't\(\)").unwrap();
+    let mul_regex = Regex::new(r"mul\([0-9]{1,3},[0-9]{1,3}\)").unwrap();
+
+    let instructions = mul_do_regex
+        .find_iter(&input)
+        .map(|f| f.as_str())
+        .collect::<Vec<_>>();
+
+    println!("Erg: {:?}", instructions);
+
+    let mut sum = 0;
+    let mut active = true;
+
+    for instruction in instructions {
+        if mul_regex.is_match(instruction) {
+            if active {
+                let (first, second) = instruction.split_once(',').unwrap();
+                let num1 = first.replace("mul(", "").parse::<i32>().unwrap();
+                let num2 = second.replace(")", "").parse::<i32>().unwrap();
+                let erg = num1 * num2;
+                sum = sum + erg;
+                println!("{} * {} = {} {}", num1, num2, erg, sum);
+            }
+        } else if instruction == "do()" {
+            active = true;
+        } else if instruction == "don't()" {
+            active = false;
+        }
+    }
+
+    sum
 }
