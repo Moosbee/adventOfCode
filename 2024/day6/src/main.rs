8bb9a83f65ed7f2e5a936902f267fbb6cfb4d11b
Moosbee <moosbeee@gmail.com>
2024-12-06 19:50:56 +0100

    day 6 part 2

--- 2024/day6/src/main.rs
+++ 2024/day6/src/main.rs
@@ -44,7 +44,7 @@ fn main() {
         direction: Direction::Up,
     };
 
-    let mut path: Vec<Vec<Position>> = input_lines
+    let path: Vec<Vec<Position>> = input_lines
         .enumerate()
         .map(|line| {
             line.1
@@ -70,109 +70,30 @@ fn main() {
         })
         .collect();
 
-    let init_guard = guard.clone();
-
-    let mut loops: HashSet<(isize, isize)> = HashSet::new();
-
-    loop {
-        let next = match guard.direction {
-            Direction::Up => (guard.y as isize - 1, guard.x as isize),
-            Direction::Down => (guard.y as isize + 1, guard.x as isize),
-            Direction::Left => (guard.y as isize, guard.x as isize - 1),
-            Direction::Right => (guard.y as isize, guard.x as isize + 1),
-        };
-
-        let is_loop = is_infinite_loop(path.clone(), guard.clone(), (next.0, next.1));
-
-        if is_loop {
-            loops.insert(next);
-            println!(
-                "Infinite loop detected at {} {} {}",
-                next.0,
-                next.1,
-                loops.len()
-            );
-        }
-
-        let next_pos = get_next_pos_mut(next.0, next.1, &mut path);
-
-        if next_pos.is_none() {
-            break;
-        }
-
-        let next_pos = next_pos.unwrap();
-
-        let has_visited = match guard.direction {
-            Direction::Up => next_pos.right,
-            Direction::Right => next_pos.down,
-            Direction::Down => next_pos.left,
-            Direction::Left => next_pos.up,
-        };
-
-        if has_visited {
-            println!("G Loop detected at {} {}", guard.x, guard.y);
-        }
-
-        if next_pos.obstacle {
-            match guard.direction {
-                Direction::Up => guard.direction = Direction::Right,
-                Direction::Right => guard.direction = Direction::Down,
-                Direction::Down => guard.direction = Direction::Left,
-                Direction::Left => guard.direction = Direction::Up,
-            };
-        } else {
-            next_pos.visited = true;
-
-            match guard.direction {
-                Direction::Up => next_pos.right = true,
-                Direction::Right => next_pos.down = true,
-                Direction::Down => next_pos.left = true,
-                Direction::Left => next_pos.up = true,
-            };
-
-            // if !(init_guard.x as isize == next.1 && init_guard.y as isize == next.0) {
-
-            // }
-
-            guard.x = next.1 as usize;
-            guard.y = next.0 as usize;
-        }
-    }
+    let count_part_1 = calc_steps(path.clone(), guard.clone());
 
     print_area(&path);
 
-    let mut count = 0;
-    for line in path.iter() {
-        for pos in line {
-            if pos.visited {
-                count += 1;
+    let mut count_part_2 = 0;
+
+    for i in 0..path.len() {
+        for j in 0..path[i].len() {
+            let mut new_path = path.clone();
+            new_path[i][j].obstacle = true;
+            let count = calc_steps(new_path.clone(), guard.clone());
+
+            println!("{} {} {}", i, j, count);
+
+            if count < 10 {
+                count_part_2 += 1;
             }
         }
     }
 
-    let mut possible_spots = path
-        .iter()
-        .enumerate()
-        .map(|x| {
-            x.1.iter()
-                .enumerate()
-                .filter(|x| !x.1.obstacle)
-                .map(|y| (x.0 as isize, y.0 as isize))
-                .collect::<Vec<_>>()
-        })
-        .flatten()
-        .collect::<HashSet<_>>();
-
-    possible_spots.remove(&(init_guard.y as isize, init_guard.x as isize));
-
-    let loops = loops.intersection(&possible_spots).collect::<Vec<_>>();
-
-    println!("Loops: {:?} ", loops);
-
     println!(
         "Part 1: {} Part 2 {}(2057 is too high) Took {:?} ",
-        count,
-        loops.len(),
+        count_part_1,
+        count_part_2,
         start.elapsed()
     )
 }
@@ -195,11 +116,8 @@ fn get_next_pos(r: isize, col: isize, path: &Vec<Vec<Position>>) -> Option<&Posi
     }
 }
 
-fn is_infinite_loop(
-    mut path: Vec<Vec<Position>>,
-    mut guard: Guard,
-    extra_obstacle: (isize, isize),
-) -> bool {
+fn calc_steps(mut path: Vec<Vec<Position>>, mut guard: Guard) -> i32 {
+    let mut count = 1;
     for _ in 0..1_000_000 {
         // for _ in 0..path.len() * path[0].len() {
         let next = match guard.direction {
@@ -212,7 +130,7 @@ fn is_infinite_loop(
         let next_pos = get_next_pos_mut(next.0, next.1, &mut path);
 
         if next_pos.is_none() {
-            return false;
+            return count;
         }
 
         let next_pos = next_pos.unwrap();
@@ -226,10 +144,10 @@ fn is_infinite_loop(
 
         if has_visited {
             println!("Loop detected at {} {}", guard.x, guard.y);
-            return true;
+            return -1;
         }
 
-        if next_pos.obstacle || (next.0 == extra_obstacle.0 && next.1 == extra_obstacle.1) {
+        if next_pos.obstacle {
             match guard.direction {
                 Direction::Up => guard.direction = Direction::Right,
                 Direction::Right => guard.direction = Direction::Down,
@@ -244,12 +162,18 @@ fn is_infinite_loop(
                 Direction::Left => next_pos.up = true,
             };
 
+            if !next_pos.visited {
+                count += 1;
+            }
+
+            next_pos.visited = true;
+
             guard.x = next.1 as usize;
             guard.y = next.0 as usize;
         }
     }
 
-    true
+    -1
 }
 
 fn print_area(path: &Vec<Vec<Position>>) {
